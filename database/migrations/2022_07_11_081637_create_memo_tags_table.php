0a204ef7714d388a67c419109f732e94abe9d8bd
carai241 <icytears932@gmail.com>
2022-07-11 17:19:26 +0900

    マイグレーションファイルの編集

--- database/migrations/2022_07_11_081637_create_memo_tags_table.php
+++ database/migrations/2022_07_11_081637_create_memo_tags_table.php
@@ -14,11 +14,8 @@ return new class extends Migration
     public function up()
     {
         Schema::create('memo_tags', function (Blueprint $table) {
-            $table->unsignedBigInteger('memo_id');
-            $table->unsignedBigInteger('tag_id');
-
-            $table->foreign('memo_id')->references('id')->on('memos');
-            $table->foreign('tag_id')->references('id')->on('tags');
+            $table->id();
+            $table->timestamps();
         });
     }
 
